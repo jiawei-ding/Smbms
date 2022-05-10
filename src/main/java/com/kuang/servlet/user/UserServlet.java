edb48473c0a013de0a70de60de3ed782888c8cde
jiaweiding <jiaweiding1998@163.com>
2022-05-10 16:11:03 +0800

    add oldPassWord authentication

--- src/main/java/com/kuang/servlet/user/UserServlet.java
+++ src/main/java/com/kuang/servlet/user/UserServlet.java
@@ -5,12 +5,16 @@ import com.kuang.service.user.UserService;
 import com.kuang.service.user.UserServiceImpl;
 import com.kuang.util.Constants;
 import com.mysql.cj.util.StringUtils;
+import com.alibaba.fastjson.JSONArray;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServlet extends HttpServlet {
     @Override
@@ -23,9 +27,12 @@ public class UserServlet extends HttpServlet {
         String method = req.getParameter("method");
         if(method != null && method.equals("savepwd")){
             this.updatePwd(req, resp);
+        }else if(method != null && method.equals("pwdmodify")){
+            this.pwdModify(req, resp);
         }
     }
 
+    //修改密码
     private void updatePwd(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -47,4 +54,44 @@ public class UserServlet extends HttpServlet {
         }
         request.getRequestDispatcher("pwdmodify.jsp").forward(request, response);
     }
+
+    //验证旧密码，seesion中有用户的密码
+    public void pwdModify(HttpServletRequest req, HttpServletResponse resp){
+        //get id from session
+        Object o = req.getSession().getAttribute(Constants.USER_SESSION);
+        String oldPassWord = req.getParameter("oldpassword");
+
+        //use hash to save result
+        Map<String, String> resultMap = new HashMap<String, String>();
+
+        if(o == null){//当session失效， 过期
+            resultMap.put("result", "sessionerror");
+        }else if(StringUtils.isNullOrEmpty(oldPassWord)){
+            //当输入密码为空
+            resultMap.put("result","error");
+        }else{
+            //得到session中用户的密码
+            String userPassword = ((User)o).getUserPassword();
+            if(oldPassWord.equals(userPassword)){
+                resultMap.put("result","true");
+            }else{
+                resultMap.put("result","false");
+            }
+        }
+
+        //将密码比对结果输出至前端
+        try {
+            resp.setContentType("application/json");
+            PrintWriter writer = resp.getWriter();
+            writer.write(JSONArray.toJSONString(resultMap));
+
+            //close
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- target/smbms-1.0-SNAPSHOT/js/pwdmodify.js
+++ target/smbms-1.0-SNAPSHOT/js/pwdmodify.js
@@ -4,42 +4,42 @@ var rnewpassword = null;
 var saveBtn = null;
 
 $(function(){
-	// oldpassword = $("#oldpassword");
+	oldpassword = $("#oldpassword");
 	newpassword = $("#newpassword");
 	rnewpassword = $("#rnewpassword");
 	saveBtn = $("#save");
 	
-	// oldpassword.next().html("*");
+	oldpassword.next().html("*");
 	newpassword.next().html("*");
 	rnewpassword.next().html("*");
 
-	// oldpassword.on("blur",function(){
-	// 	$.ajax({
-	// 		type:"GET",
-	// 		url:path+"/jsp/user.do",
-	// 		data:{method:"pwdmodify",oldpassword:oldpassword.val()},
-	// 		dataType:"json",
-	// 		success:function(data){
-	// 			if(data.result == "true"){//旧密码正确
-	// 				validateTip(oldpassword.next(),{"color":"green"},imgYes,true);
-	// 			}else if(data.result == "false"){//旧密码输入不正确
-	// 				validateTip(oldpassword.next(),{"color":"red"},imgNo + " 原密码输入不正确",false);
-	// 			}else if(data.result == "sessionerror"){//当前用户session过期，请重新登录
-	// 				validateTip(oldpassword.next(),{"color":"red"},imgNo + " 当前用户session过期，请重新登录",false);
-	// 			}else if(data.result == "error"){//旧密码输入为空
-	// 				validateTip(oldpassword.next(),{"color":"red"},imgNo + " 请输入旧密码",false);
-	// 			}
-	// 		},
-	// 		error:function(data){
-	// 			//请求出错
-	// 			validateTip(oldpassword.next(),{"color":"red"},imgNo + " 请求错误",false);
-	// 		}
-	// 	});
-	//
-	//
-	// }).on("focus",function(){
-	// 	validateTip(oldpassword.next(),{"color":"#666666"},"* 请输入原密码",false);
-	// });
+	oldpassword.on("blur",function(){//当旧密码失去焦点时，就执行以操作
+		$.ajax({
+			type:"GET",
+			url:path+"/jsp/user.do",
+			data:{method:"pwdmodify",oldpassword:oldpassword.val()},
+			dataType:"json",
+			success:function(data){
+				if(data.result == "true"){//旧密码正确
+					validateTip(oldpassword.next(),{"color":"green"},imgYes,true);
+				}else if(data.result == "false"){//旧密码输入不正确
+					validateTip(oldpassword.next(),{"color":"red"},imgNo + " 原密码输入不正确",false);
+				}else if(data.result == "sessionerror"){//当前用户session过期，请重新登录
+					validateTip(oldpassword.next(),{"color":"red"},imgNo + " 当前用户session过期，请重新登录",false);
+				}else if(data.result == "error"){//旧密码输入为空
+					validateTip(oldpassword.next(),{"color":"red"},imgNo + " 请输入旧密码",false);
+				}
+			},
+			error:function(data){
+				//请求出错
+				validateTip(oldpassword.next(),{"color":"red"},imgNo + " 请求错误",false);
+			}
+		});
+
+
+	}).on("focus",function(){
+		validateTip(oldpassword.next(),{"color":"#666666"},"* 请输入原密码",false);
+	});
 	
 	newpassword.on("focus",function(){
 		validateTip(newpassword.next(),{"color":"#666666"},"* 密码长度必须是大于6小于20",false);
